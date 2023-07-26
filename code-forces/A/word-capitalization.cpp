8da9b3704c2c95b47877ac2ef1ca32e9c98cb8fe
Pedro Martins <100642860+pedromartinssilva@users.noreply.github.com>
2023-07-26 18:16:48 -0300

    Create word-capitalization.cpp

---
+++ code-forces/A/word-capitalization.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+ 
+using namespace std;
+ 
+int main()
+{
+    string word;
+ 
+    cin >> word;
+ 
+    word[0] = toupper(word[0]);
+ 
+    cout << word;
+ 
+    return 0;
+}
