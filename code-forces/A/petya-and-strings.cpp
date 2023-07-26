d00e38db2ade9af51169fc4b0ac8f81690bc9f42
Pedro Martins <100642860+pedromartinssilva@users.noreply.github.com>
2023-07-26 18:19:42 -0300

    Create petya-and-strings.cpp

---
+++ code-forces/A/petya-and-strings.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+ 
+using namespace std;
+ 
+int main() {
+    string str1, str2;
+ 
+    cin >> str1 >> str2;
+ 
+    transform(str1.begin(), str1.end(), str1.begin(), ::tolower);
+    transform(str2.begin(), str2.end(), str2.begin(), ::tolower);
+ 
+    if (str1<str2) {
+        cout << -1 << endl;
+    } else if (str2<str1) {
+        cout << 1 << endl;
+    } else {
+        cout << 0 << endl;
+    }
+ 
+    return 0;
+}
